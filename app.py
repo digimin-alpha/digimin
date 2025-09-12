968767bf4920dce18fc88bbb32e507f19aab3b52
digimin-alpha <atdrossman@gmail.com>
2025-09-12 17:39:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def hello_world():
 if __name__ == '__main__':
     # Run the app and set its "public street address" to 0.0.0.0,
     # so that the Render service can connect to it.
-    app.run(host='0.0.0.0', port=5000)er service can connect to it.
     app.run(host='0.0.0.0', port=5000)
+host='0.0.0.0', port=5000)
